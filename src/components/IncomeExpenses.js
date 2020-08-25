34455b69e73306e94a0c230a9e912ca8fe8ba320
Adetayo <akinsanyaadetayo7@gmail.com>
2020-08-25 04:58:36 +0100

    👌 Chore: Add the income and expense calcculation logic

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const IncomeExpenses = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const amount = transactions.map((transaction) => transaction.amount);
+
+  const income = amount
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expenses = amount
+    .filter((item) => item < 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
   return (
     <div className='inc-exp-container'>
       <div>
         <h4>Income</h4>
         <p className='money plus'>
-          +<span>&#8358;</span>0.00
+          +<span>&#8358;</span>
+          {income}
         </p>
       </div>
       <div>
         <h4>Expense</h4>
         <p className='money minus'>
-          -<span>&#8358;</span>0.00
+          -<span>&#8358;</span>
+          {expenses}
         </p>
       </div>
     </div>
